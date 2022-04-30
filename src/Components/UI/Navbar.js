aaca669c257656ce86e32dbf845ff57527688b5a
ricardonyc <rvardo98@gmail.com>
2022-04-30 02:01:06 -0400

    added navbar

---
+++ src/Components/UI/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+import CoolSchoolsSVG from "../assets/CoolSchools.svg";
+
+function Navbar(props) {
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.nav__container}>
+        <img className={classes.logo} src={CoolSchoolsSVG} alt="" />
+        {/* <h1 className={classes.logo}>CoolSchools</h1> */}
+        <ul className={classes.nav__menu}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
