124cb735af82701a4379f5b58ef4d0a87ae06963
Ricardo Vargas <rvardo98@gmail.com>
2022-05-01 01:47:16 -0400

    created Firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp, getAuth } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
