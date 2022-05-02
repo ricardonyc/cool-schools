a0936c8c577210da367cce1052b804bb2b464a64
ricardonyc <rvardo98@gmail.com>
2022-05-02 16:30:44 -0400

    edited dark mode h1

--- src/Components/UI/Boxes.js
+++ src/Components/UI/Boxes.js
@@ -1,19 +1,31 @@
-import classes from "./Boxes.module.css";
+import css from "./Boxes.module.css";
 import AnonBro from "../assets/bro2.svg";
 import Working from "../assets/working.svg";
+import { ThemeContext } from "../../darkmode-context";
+import { useContext } from "react";
 
 const Boxes = () => {
+  const { darkMode } = useContext(ThemeContext);
+
   return (
-    <div className={classes["boxes__container"]}>
-      <div className={classes.box}>
+    <div className={css["boxes__container"]}>
+      <div
+        className={
+          darkMode ? `${css.box} ${css.light}` : `${css.box} ${css.dark}`
+        }
+      >
         <img src={Working} alt="man using laptop in a forest" />
         <h1>Leave Reviews</h1>
       </div>
-      <div className={classes.box}>
+      <div
+        className={
+          darkMode ? `${css.box} ${css.light}` : `${css.box} ${css.dark}`
+        }
+      >
         <img src={AnonBro} alt="Anonymous looking eyes" />
         <h1>Remain anonymous</h1>
       </div>
-      {/* <div className={classes.box}></div> */}
+      {/* <div className={css.box}></div> */}
     </div>
   );
 };
