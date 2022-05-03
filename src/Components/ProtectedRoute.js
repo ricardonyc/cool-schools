40ef965de7e14141896749b578e9d6aff4ce67a9
ricardonyc <rvardo98@gmail.com>
2022-05-03 00:48:28 -0400

    fixed protected component

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,17 +1,26 @@
+import { useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
+import { auth } from "../firebase";
 
 const ProtectedRoute = ({ children }) => {
-  let { user } = useUserAuth();
+  //   let { user } = useUserAuth();
+  const [user, setUser] = useState({});
 
-  console.log("user: ", user.uid);
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  //   console.log("user: ", user.uid);
 
   // WHEN PAGE IS REFRESHED, WE GET SIGNED OUT BECAUSE THE INITAL STATE OF 'USER' IS EMPTY
   // SO THIS BECOMES TRUE AND IT SENDS US TO LOGGED OUT HOME PAGE
-  if (!user.uid === "undefined") {
+  // ? TEST TO SEE IF IT WORKS
+  // * SEEMS LIKE ITS BEEN FIXED!
+  if (!user) {
     return <Navigate to="/" />;
   }
-
 
   return <div>{children}</div>;
 };
