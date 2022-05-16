294e49fa802a5488cda089cd0e717c988b641453
ricardonyc <rvardo98@gmail.com>
2022-05-15 23:39:46 -0400

    added icon to footer

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import css from "./styling/Footer.module.css";
 import { ThemeContext } from "../../context/DarkModeContext";
 import { Link } from "react-router-dom";
+import { FaLinkedin } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -22,12 +24,16 @@ const Footer = () => {
           <div className={css.menu__box}>
             <Link to="/">Home</Link>
             <Link to="/addschool">Add School</Link>
-            {/* <Link to="/about">About</Link> */}
+            <a
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/ricardo67/"
+              target="_blank"
+            >
+              <FaLinkedin style={{ fontSize: "3rem" }} />
+            </a>
           </div>
-          {/* <div className={css.menu__box}> */}
-            {/* <p>Login</p> */}
-            {/* <p>Sign Up</p> */}
-          {/* </div> */}
+          {/* <div className={css.menu__box}>
+          </div> */}
         </div>
       </div>
     </footer>
